f365217f1c312b0851601fd7879284d3a2835549
eun <chocoding0@naver.com>
2021-11-02 00:54:48 +0900

    main

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="MnM logo" src="./assets/logo.png">
+  <HelloWorld msg="하우스메이트 매칭 및 공동 생활 관리 서비스 제공 플랫폼"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import main_page from './components/.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main_page
   }
 }
 </script>
 
 <style>
 #app {
+  width: 100%;
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000000;
+  background-color: #EABF3A;
+  padding: 0;
+  margin: 0;
 }
+
 </style>
---
+++ src/components/main_page.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h2>{{ msg }}</h2>
+    <p>
+      <img alt="Concept Image" src="../assets/concept_image.png">
+    </p>
+    <button> log in </button>
+    <button> log out </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main_page',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  margin: 40px 0 0;
+}
+button {
+  width: 148px;
+  height: 51px;
+  font-size: 20px;
+  background-color: red;
+  color: black;
+  border: solid 2px black;
+  border-radius: 16px;
+  margin: 5px;
+}
+img {
+  width: 268px;
+  height: 558px;
+}
+</style>
