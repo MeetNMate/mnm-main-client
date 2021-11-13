d484831f5eaf259ccfb34098c32a6c6faa9c8160
eun <chocoding0@naver.com>
2021-11-14 01:05:39 +0900

    mainpage_1114

---
+++ src/LoginPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <mini-logo></mini-logo>
+  
+</template>
+
+<script>
+import MiniLogo from './components/mini-logo.vue'
+import Main from './components/red-button.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Main,
+    MiniLogo,
+    
+  }
+}
+</script>
+
+<style>
+#loginpage {
+  width: 100%;
+  height: 100%;
+  height: 100vh;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+  background-color: #EABF3A;
+  padding: 0;
+  margin: 0;
+}
+
+template {
+  background-color: #EABF3A;
+}
+
+</style>
---
+++ src/components/main-img.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-img">
+    <p>
+      <img alt="Concept Image" src="../assets/concept_image.png">
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main-img',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  width: 268px;
+  height: 558px;
+}
+</style>
