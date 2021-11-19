be403130af801ad5aeaefcb227467f3369dca6fb
obok2 <tjrgml0731@daum.net>
2021-11-20 03:32:35 +0900

    navigator 형태만 대충 만듦 : 수정 필요

--- src/main.js
+++ src/main.js
@@ -1,65 +1,49 @@
 // import Vue from 'vue'
 import { createApp } from 'vue'
-// import Router from 'vue-router'
-import MainPage from './MainPage.vue'
-import LoginPage from './LoginPage.vue'
-import RegisterPage from './RegisterPage.vue'
-import MatchingWaitPage from './MatchingWaitPage.vue'
+import Main from './Main.vue'
+import router from './router'
+
+// import App from './App.vue'
+
+import MainPage from './views/MainPage.vue'
+import LoginPage from './views/LoginPage.vue'
+import RegisterPage from './views/RegisterPage.vue'
+
+import InfoExplanePage from './views/InfoExplanePage.vue'
+import InfoInputPage from './views/InfoInputPage.vue'
+import MatchingWaitPage from './views/MatchingWaitPage.vue'
+import MateListPage from './views/MateListPage.vue'
+
 import HouseLobby from './house-lobby.vue'
 import HouseRule from './house-rule.vue'
 import HouseReport from './house-report.vue'
 import HouseMemReport from './house-mem-report.vue'
 import HouseMemContent from './house-mem-content.vue'
 import Navigator from './components/navigator.vue'
-import NavBar from './components/navbar.vue'
 import RuleModal from './components/common/Modal.vue'
-// import Content from './components/common/modal_content.vue'
-// import MemReport from './components/common/modal_report.vue'
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
+// import DropdownMenu from '@innologica/vue-dropdown-menu'
 
-
-createApp(MainPage).mount('#mainpage')
-createApp(LoginPage).mount('#loginpage')
-createApp(RegisterPage).mount('#registerpage')
-createApp(MatchingWaitPage).mount('#matchingwaitpage')
-createApp(HouseLobby).mount('#houselobby')
-createApp(HouseRule).mount('#houserule')
+createApp(Main).use(router).mount('#main')
+createApp(MainPage).use(router).mount('#mainpage')
+createApp(LoginPage).use(router).mount('#loginpage')
+createApp(RegisterPage).use(router).mount('#registerpage')
+createApp(InfoExplanePage).use(router).mount('#infoexplanepage')
+createApp(InfoInputPage).use(router).mount('#infoinputpage')
+createApp(MatchingWaitPage).use(router).mount('#matchingwaitpage')
+createApp(MateListPage).use(router).mount('#matelistpage')
+createApp(HouseLobby).use(router).mount('#houselobby')
+createApp(HouseRule).use(router).mount('#houserule')
 createApp(HouseReport).mount('#housereport')
 createApp(HouseMemReport).mount('#housememreport')
 createApp(HouseMemContent).mount('#housememcontent')
 createApp(Navigator).mount('#navi')
-createApp(NavBar).mount('#navbar')
 createApp(RuleModal).mount('#rulemodal')
-// createApp(Content).mount('#content')
-// createApp(MemReport).mount('#memreport')
+// createApp(DropdownMenu).mount('#dropdownmenu')
 
-
-// Vue.use(Router)
-//
-// export default new Router({
-//   mode: 'history',    //아니면 hash
-//   routes: [
-//     {
-//       path: '/house-lobby.vue',
-//       name: 'house list',
-//       component: HouseLobby
-//     }
-//   ]
-// })
-
-// export default new Router({
-//   mode: 'history',
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'house list',
-//       component: HouseLobby
-//     }
-//   ]
-// })
-// Vue.config.productionTip = false
-//
-// new Vue({
-//   render: h => h(App),
-// }).$mount('#app')
+// Vue.component('vue-multiselect', window.VueMultiselect.default)
+// const app = createApp(App)
+// app.component("vue3-star-ratings", vue3StarRatings)
