e0e1ddaf315d28c0726e1c567b406832646066e2
obok2 <tjrgml0731@daum.net>
2021-11-19 21:26:26 +0900

    house report page without router

--- src/house-report.vue
+++ src/house-report.vue
@@ -7,7 +7,7 @@
       <img id="title" src="./assets/assessment_title.png">
     </div>
 
-    <div class="list_table">
+    <div class="report_table">
       <table>
         <colgroup>
           <col width="20%">
@@ -15,11 +15,38 @@
           <col width="20%">
         </colgroup>
         <tbody>
-          <tr v-for="item in items" :key="item.profile">
-            <td><img v-bind:src="item.profile"></td>
-            <td><span v-html="item.user"></span></td>
-            <!-- <td><span class="plusMate" v-on:click="addMate">{{ item.link }}</span></td> -->
-            <td><span v-on:click="startReport"><img v-bind:src="item.button"></span></td>
+          <tr id="table_head">
+            <td rowspan="2">
+              <span class="profile">
+                <img id="profile_img" src='./assets/profile_2.png'>
+              </span>
+            </td>
+            <td id="user">moosongsong</td>
+            <td rowspan="2">
+              <span class="report_button" v-on:click="ReportBtn">
+                <img id="btn_report" src='./assets/report_btn.png'>
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td id="report_status">평가 완료</td>
+          </tr>
+
+          <tr id="table_head">
+            <td rowspan="2">
+              <span class="profile">
+                <img id="profile_img" src='./assets/profile_2.png'>
+              </span>
+            </td>
+            <td id="user">moosongsong</td>
+            <td rowspan="2">
+              <span class="report_button" v-on:click="ReportBtn">
+                <img id="btn_report" src='./assets/report_btn.png'>
+              </span>
+            </td>
+          </tr>
+          <tr id="table_bottom">
+            <td id="report_status">평가 대기 중...</td>
           </tr>
         </tbody>
       </table>
@@ -38,49 +65,21 @@ export default {
     minilogo
   },
   methods: {
-    startReport() {
+    ReportBtn() {
       console.log();
-    }
-  },
-  data() {   //수정 필수 !! //
-    return {
-      items: [
-        {
-          "profile": "https://ww.namu.la/s/89b05ae4e797bc096c37dd2d872ca37830de5804b01d209f77efa10661cba9243bc4b2f88f97a0832bba69c2b95deb59fafe8571adaf8439244b46da5cde129067742b3bf0a72cb97c568f20f0d9a6a52fe93929137161c13cab8b5c71a02605bbceb53cf0b1b168fda31a3e56605248",
-          "user" : "moosongsong << 평가 완료 >>",
-          "button" : "https://ww.namu.la/s/89b05ae4e797bc096c37dd2d872ca37830de5804b01d209f77efa10661cba9243bc4b2f88f97a0832bba69c2b95deb59fafe8571adaf8439244b46da5cde129067742b3bf0a72cb97c568f20f0d9a6a52fe93929137161c13cab8b5c71a02605bbceb53cf0b1b168fda31a3e56605248"
-        },
-        {
-          "profile": "https://w.namu.la/s/116f07b5cfaf907bad8e0652f3b576e5e77caa2bf9d2a6679e0d1c3a27034142fee25fd6ea95bee99811e4de63ff9be7eb194f1d3a2dd392dc12227a58f00a23d3373957e76b4b644aecf75938a22d2272094585c176cdee1c5d6a2c72881775c71219cb10265e4acacf50e03f679820",
-          "user" : "seoki << 평가 대기 중... >>",
-          "button" : "https://w.namu.la/s/116f07b5cfaf907bad8e0652f3b576e5e77caa2bf9d2a6679e0d1c3a27034142fee25fd6ea95bee99811e4de63ff9be7eb194f1d3a2dd392dc12227a58f00a23d3373957e76b4b644aecf75938a22d2272094585c176cdee1c5d6a2c72881775c71219cb10265e4acacf50e03f679820"
-        }
-      ]
     }
   }
 }
 </script>
 
 <style scoped>
-  img {
-    width: 70px;
-    height: 70px;
-    padding: 0 10px 0 10px;
-  }
   table {
     border-collapse: collapse;
-    border-top: 1px solid black;
-    margin-left: auto;
-    margin-right: auto;
-  }
-  tr {
-    border-bottom: 1px solid black;
   }
   td {
-    padding: 10px 0 10px 0;
-  }
-  span {
-    font-family: a고딕19;
+    font-family: a고딕14;
+    font-size: 18px;
+    padding-left: 10px;
   }
   #page_title {
     text-align: center;
@@ -90,9 +89,27 @@ export default {
     width: 218px;
     height: 40px;
   }
-  .list_table {
-    padding: 0 10px 0 10px;
-
+  #table_head {
+    border-top: 1px solid black;
   }
-
+  #profile_img {
+    width: 70px;
+    height: 70px;
+    padding: 10px 5px 5px 10px;
+  }
+  #user {
+    font-family: a고딕19;
+    font-size: 20px;
+  }
+  #btn_report {
+    width: 63px;
+    height: 25px;
+    padding-right: 20px;
+  }
+  #table_head {
+    border-top: 1px solid black;
+  }
+  #table_bottom {
+    border-bottom: 1px solid black;
+  }
 </style>
