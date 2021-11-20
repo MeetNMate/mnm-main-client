e590e8b97240ea1f501fb0b126bf9bbbca0b0972
obok2 <tjrgml0731@daum.net>
2021-11-21 02:19:50 +0900

    ugly navigation bar..(not bootstrap)

--- src/components/navigator.vue
+++ src/components/navigator.vue
@@ -1,74 +1,71 @@
 <template>
-  <div class="dropdown">
-    <button
-      class="btn dropdown-toggle"
-      type="button"
-      id="dropdownMenuButton1"
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-    >
-      <img id="navbar" src='./arrow_down.png'>
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-      <li id="navi_list"><a class="dropdown-item" href="/auth/matelist">mate list</a></li>
-      <li id="navi_list"><a class="dropdown-item" href="/auth/chattinglist">chatting list</a></li>
-      <li id="navi_list"><a class="dropdown-item" href="/auth/house/lobby">house list</a></li>
-      <li id="navi_list"><a class="dropdown-item" href="/auth/mypage">my page</a></li>
-    </ul>
+  <div class="menu_drop">
+    <button id="dropmenu" @click="isOpen = !isOpen"><img src="./arrow_down.png"></button>
+    <nav class="navmenu" v-if="isOpen">
+      <ul class="menu">
+        <li id="navi_list"><a class="dropdown-item" href="/auth/matelist">mate list</a></li>
+        <li id="navi_list"><a class="dropdown-item" href="/auth/chattinglist">chatting list</a></li>
+        <li id="navi_list"><a class="dropdown-item" href="/auth/house/lobby">house list</a></li>
+        <li id="navi_list"><a class="dropdown-item" href="/auth/mypage">my page</a></li>
+      </ul>
+    </nav>
+
   </div>
 </template>
 
 <script>
+import barform from './bar.vue'
+
   export default {
-    name: 'dropdown'
+    name: 'navimenu',
+    components: {
+      barform
+    },
+    data() {
+      return {
+        isOpen: false
+      }
+    }
   }
 </script>
 
+
 <style scoped>
-  .dropdown {
-    padding-bottom: 30px;
-  }
-  #navbar {
-    width: 25px;
-    height: 19px;
+  .menu_drop {
+    padding-bottom: 20px;
     text-align: center;
-    padding: none;
   }
-  .dropdown-menu {
-    width: 100%;
-    border-radius: 0;
-  }
-  .dropdown-menu,
-  #navi_list {
-    background-color: #E88827;
-  }
-  #navi_list {
-    list-style-type: none;
-    border-bottom: 1px solid black;
-  }
-  #navi_list:focus {
-    text-decoration: none;
-  }
-  button {
-    background-color: #E88827;
-    border-radius: 0;
-  }
-  .btn {
+  #dropmenu {
     width: 100%;
     height: 37px;
-    padding-left: 0;
-    padding-right: 0;
+    background-color: #E88827;
+    border: 0;
+    border-top: 1px solid black;
+    border-bottom: 1px solid black;
   }
-  button:focus {
-    box-shadow: none;
+  .navmenu {
+    background-color: #E88827;
   }
-  a {
+  #navi_list {
+    height: 37px;
+    list-style-type: none;
+    /* line-height: 30%; */
+    margin-right: 35px;
+
+  }
+  .dropdown-item {
     font-family: a고딕14;
     font-size: 20px;
-    text-align: center;
-  }
-  a.over {
     text-decoration: none;
-    background-color: #E88827;
+    color: black;
+  }
+  .dropdown-item:visited {
+    color: black;
+  }
+  .dropdown-item:hover {
+    color: black;
+  }
+  .dropdown-item:active {
+    color: black;
   }
 </style>
